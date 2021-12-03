a8651ab153bd151bb58cfb54a263f813019c7b55
sumi005 <salli7635@richmondhillhs.org>
2021-12-03 16:30:08 +0000

    some message describing changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import PetNav from './components/pet-nav'
-import PictureBox from './components/picture-box'
+import PetNav from './components/pet-nav';
+import PictureBox from './components/picture-box';
+import AboutBox from './components/about-box';
+import Posts from './components/posts';
 import './App.css';
 
 const App = () => {
@@ -12,16 +14,17 @@ const App = () => {
         <PetNav/>
         <div class="container">
           <div class="row">
-            //Add profile picture and about section here
+              <PictureBox/>
+              <AboutBox/>
           </div>
           <div class="row">
-            //Add posts here
+            <Posts/>
           </div>
         </div>
       </div>
     )
   }
-
+  
   return component
 }
 
--- src/components/picture-box.js
+++ src/components/picture-box.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import pic from './images/takota.png'
-import './css/picture-box.css'
+import pic from './images/takota.png';
+import './css/picture-box.css';
 
 
 const PictureBox = () => {
     return (
-      <div className='PictureBox'>
-
+        <div class="col-4 picture-box">
+        <img src={pic} class="pic"/>
+        <h3>Takota McConner</h3>
+        <p class="list-group-item">"Laziness is nothing more than the habit of resting before you get tired." -Jules Renard</p>
       </div>
     );
 }
 
-export default PictureBox
+export default PictureBox;
